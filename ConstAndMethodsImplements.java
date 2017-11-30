c581969d0906d3b8f821af98e0a0cb887eaad46f
Dmitry Pinchuk <pinchuk.dap@gmail.com>
2017-11-30 19:13:49 +0200

    Update ConstAndMethodsImplements.java

--- ConstAndMethodsImplements.java
+++ ConstAndMethodsImplements.java
@@ -4,5 +4,5 @@ public interface ConstAndMethodsImplements {
     // Count of points
     byte COUNT_OF_POINTS = 11;
     // Path to write.
-    String path = "D:\\project4_2.txt";
+    String path = "\project4_2.txt";
 }
